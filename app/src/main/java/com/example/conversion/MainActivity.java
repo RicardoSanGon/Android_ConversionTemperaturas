e5837e97467b184dfb1068c3898e998ced6357b6
RicardoSanGon <22170038@uttcampus.edu.mx>
2023-10-11 07:02:09 -0600

    Primera version 1.0

---
+++ app/src/main/java/com/example/conversion/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.conversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import com.example.conversion.models.Celsius;
+
+public class MainActivity extends AppCompatActivity {
+    TextView Cargando;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Cargando=findViewById(R.id.txtCargando);
+
+        new CountDownTimer(5000,1000)
+        {
+            @Override
+            public void onTick(long l)
+            {
+                //
+            }
+            @Override
+            public void onFinish()
+            {
+                activity2();
+            }
+        }.start();
+    }
+    private void activity2()
+    {
+        Intent i=new Intent(this,Convertidor.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/conversion/models/Celsius.java
@@ -0,0 +1,18 @@
+package com.example.conversion.models;
+
+public class Celsius extends Grado{
+    public Celsius(Double valorC) {
+        this.setUnidad("°C");
+        this.setValor(valorC);
+    }
+    public Celsius parse(Farenheit f)
+    {
+        Double c=(f.getValor()-32)*5/9;
+        return new Celsius(c);
+    }
+    public Celsius parse(Kelvin k)
+    {
+        Double c=k.getValor()-273.15;
+        return new Celsius(c);
+    }
+}
